b7ec9538141c036951e97b8106ac0c522f54485b
samuelEvans000 <chintuvincent@gmail.com>
2023-11-08 22:09:54 +0530

    modified

--- src/components/quote/quote.jsx
+++ src/components/quote/quote.jsx
@@ -1,40 +1,38 @@
+
 import  { useEffect, useState } from 'react';
 import axios from 'axios';
 import Headers from '../Header/Header'
 import './quote.css'
 
 const Quote = () => {
-    const [quote, setQuote] = useState({ content: '', author: '' });
-  
-  useEffect(() => {
-    async function fetchQuote() {
-      try {
-          
-          const response = await axios.get('https://api.quotable.io/random');
-          const { content, author } = response.data;
-        setQuote({
-          content,
-          author,
-        });
-      } catch (error) {
-        console.error('Error fetching quote:', error);
-      }
-    }
-    
-    fetchQuote();
-}, []);
+  const [quote, setQuote] = useState({ content: '', author: '' });
 
-// const refreshPage =  window.location.reload(false);
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://api.quotable.io/random');
+      const { content, author } = response.data;
+      setQuote({
+        content,
+        author,
+      });
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
   return (
     <>
     <Headers/>
-    <div className='container'>
-        <p className='quote'>{quote.content}</p>
-        <p className='author'>-{quote.author}</p> 
-    </div>
+      <div className="container">
+        <p className="quote">{quote.content}</p>
+        <p className="author">-{quote.author}</p>
+      </div>
     <div className='next-button'>
-        <button className='next' >next Quote</button>
+        <button className='next' onClick={fetchQuote} >next Quote</button>
     </div>
     </>
   );
